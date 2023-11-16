ef7a635c3060fc5cf2f9dfe1e7af3a78eda11ad6
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 18:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 //your JS code here. If required.
-    var loginForm = document.getElementById('loginForm');
-
-    loginForm.addEventListener('submit', function (event) {
-        var firstName = document.getElementById('fname').value;
-        var lastName = document.getElementById('lname').value;
-        var phoneNumber = document.getElementById('pnumber').value;
-        var email = document.getElementById('email').value;
-
-        var message = "First Name: " + firstName + "\n" +
-                      "Last Name: " + lastName + "\n" +
-                      "Phone Number: " + phoneNumber + "\n" +
-                      "Email ID: " + email;
-
-        alert(message);
-    });
+var loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", (event) => {
+	alert(
+		 `Frist Name: ${document.getElementById("fname").value}\nLast Name: ${document.getElementById("lname").value}\nPhone Number: ${document.getElementById("pnumber").value}\nEmail: ${document.getElementById("email").value}`
+	)
+});
