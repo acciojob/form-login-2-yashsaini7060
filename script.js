0c386e0e204fdabeeccf3a9a9671fd29a410046b
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 18:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 var loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", (event) => {
 	alert(
-		 `Frist Name: ${document.getElementById("fname").value}\nLast Name: ${document.getElementById("lname").value}\nPhone Number: ${document.getElementById("pnumber").value}\nEmail: ${document.getElementById("email").value}`
+		 `Frist Name: ${document.getElementById("fname").value}\nLast Name: ${document.getElementById("lname").value}\nPhone Number: ${document.getElementById("pnumber").value}\nEmail ID: ${document.getElementById("email").value}`
 	)
 });
