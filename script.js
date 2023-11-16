507260a767986cc6eddf134c4508059519422a61
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 17:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+// document.addEventListener('DOMContentLoaded', function () {
+    var loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // prevent the default form submission
+
+        var firstName = document.getElementById('fname').value;
+        var lastName = document.getElementById('lname').value;
+        var phoneNumber = document.getElementById('pnumber').value;
+        var email = document.getElementById('email').value;
+
+        var message = "First Name: " + firstName + "\n" +
+                      "Last Name: " + lastName + "\n" +
+                      "Phone Number: " + phoneNumber + "\n" +
+                      "Email ID: " + email;
+
+        alert(message);
+    });
+// });
