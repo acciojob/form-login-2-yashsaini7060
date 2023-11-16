be5c6c0a4cddbb3ff46b60594ff406f83c9af258
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 17:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 //your JS code here. If required.
-// document.addEventListener('DOMContentLoaded', function () {
     var loginForm = document.getElementById('loginForm');
 
     loginForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // prevent the default form submission
-
         var firstName = document.getElementById('fname').value;
         var lastName = document.getElementById('lname').value;
         var phoneNumber = document.getElementById('pnumber').value;
@@ -17,4 +14,3 @@
 
         alert(message);
     });
-// });
